710def068e8631a5cf98b65eca0f6bddf9f724b4
Inglide Noberto <inglidenoberto961@gmail.com>
2023-03-07 14:36:26 -0300

    Adicionar arquivos de projeto.

---
+++ AtivEnum/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AtivEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter departments name: ");
+            string departName = Console.ReadLine();
+
+            Console.WriteLine("Enter worker data: ");  
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Level (Junior/MidLevel/Senior)");
+            // provavelmente aqui vem um enum
+
+            Console.Write("Base salary");
+            double salary = double.Parse(Console.ReadLine());
+
+            Console.Write("How many contracts to this worker");
+            int qtdWork = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
